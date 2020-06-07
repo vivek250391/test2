e57a4615aa72665795292943f8e9d18421279451
vivek250391 <65803436+vivek250391@users.noreply.github.com>
2020-06-07 12:38:49 +0530

    added service
    added service

---
+++ WebApplication1/Contracts/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Contracts
+{
+    public interface IProduct
+    {
+        List<ProductModel> getProducts();
+    }
+}
