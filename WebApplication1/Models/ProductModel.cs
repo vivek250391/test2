2ad9768be9bf6d7ccfc8ba8cfa3e3eb31f96a3cd
vivek250391 <65803436+vivek250391@users.noreply.github.com>
2020-06-07 12:37:44 +0530

    added product
    added product

---
+++ WebApplication1/Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public decimal CostPrice { get; set; }
+    }
+}
